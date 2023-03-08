941f78c806c8260f6b2a8928138cd6fcc57e02f0
Neikan <sladkov.e.m@outlook.com>
2023-03-08 21:21:03 +0300

    feat(consts): add common

---
+++ lib/consts/common.dart
@@ -0,0 +1,8 @@
+// Package imports:
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+const Point pointHome = Point(latitude: 47.2313500, longitude: 39.7232800);
+
+const double scaleMin = 0.0;
+const double scaleMax = 16.0;
+const double scaleStart = 8.0;
